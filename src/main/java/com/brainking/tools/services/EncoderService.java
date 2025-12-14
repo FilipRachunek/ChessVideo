c359739e6053965f50137a113ae2bc556a6d83a3
Filip Rachůnek <Filip.Rachunek@gmail.com>
2025-12-14 18:41:22 +0100

    EncoderService refactored.

--- src/main/java/com/brainking/tools/services/EncoderService.java
+++ src/main/java/com/brainking/tools/services/EncoderService.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ws.schild.jave.Encoder;
+import ws.schild.jave.EncoderException;
 import ws.schild.jave.MultimediaObject;
 import ws.schild.jave.encode.AudioAttributes;
 import ws.schild.jave.encode.EncodingAttributes;
@@ -14,6 +15,7 @@ import ws.schild.jave.encode.enums.X264_PROFILE;
 import ws.schild.jave.info.VideoSize;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 @Service
@@ -21,46 +23,47 @@ public class EncoderService {
 
     private static final Logger LOG = LoggerFactory.getLogger(EncoderService.class);
 
-    public String convertToMP4(String targetFolder, String videoName) {
+    public String convertToMP4(final String targetFolder, final String videoName) {
+        String result = "";
         try {
-            File source = new File(targetFolder, videoName + ".mov");
-            File target = new File(targetFolder, videoName + ".mp4");
-            VideoAttributes video = new VideoAttributes();
+            final File source = new File(targetFolder, videoName + ".mov");
+            final File target = new File(targetFolder, videoName + ".mp4");
+            final VideoAttributes video = new VideoAttributes();
             video.setCodec("h264");
             video.setX264Profile(X264_PROFILE.MAIN);
             video.setFrameRate(25);
             video.setSize(new VideoSize(Constants.VIDEO_WIDTH, Constants.VIDEO_HEIGHT));
-            EncodingAttributes attrs = new EncodingAttributes();
+            final EncodingAttributes attrs = new EncodingAttributes();
             attrs.setOutputFormat("mp4");
             attrs.setVideoAttributes(video);
-            Encoder encoder = new Encoder();
+            final Encoder encoder = new Encoder();
             encoder.encode(new MultimediaObject(source), target, attrs);
             FileUtils.forceDelete(source);
-            return targetFolder + "/" + videoName + ".mp4";
-        } catch (Exception ex) {
+            result = targetFolder + "/" + videoName + ".mp4";
+        } catch (EncoderException | IOException ex) {
             LOG.error("Error converting the video to MP4.", ex);
-            return null;
         }
+        return result;
     }
 
-    public void addAudioToVideo(String audioPath, String videoPath, String targetPath) {
+    public void addAudioToVideo(final String audioPath, final String videoPath, final String targetPath) {
         try {
-            File audio = new File(audioPath);
-            File video = new File(videoPath);
-            File target = new File(targetPath);
-            AudioAttributes audioAttributes = new AudioAttributes();
-            VideoAttributes videoAttributes = new VideoAttributes();
+            final File audio = new File(audioPath);
+            final File video = new File(videoPath);
+            final File target = new File(targetPath);
+            final AudioAttributes audioAttributes = new AudioAttributes();
+            final VideoAttributes videoAttributes = new VideoAttributes();
             videoAttributes.setCodec("h264");
             videoAttributes.setX264Profile(X264_PROFILE.MAIN);
             videoAttributes.setFrameRate(25);
             videoAttributes.setSize(new VideoSize(Constants.VIDEO_WIDTH, Constants.VIDEO_HEIGHT));
-            EncodingAttributes attrs = new EncodingAttributes();
+            final EncodingAttributes attrs = new EncodingAttributes();
             attrs.setOutputFormat("mp4");
             attrs.setAudioAttributes(audioAttributes);
             attrs.setVideoAttributes(videoAttributes);
-            Encoder encoder = new Encoder();
+            final Encoder encoder = new Encoder();
             encoder.encode(Arrays.asList(new MultimediaObject(audio), new MultimediaObject(video)), target, attrs);
-        } catch (Exception ex) {
+        } catch (EncoderException ex) {
             LOG.error("Error adding audio to the video.", ex);
         }
     }
