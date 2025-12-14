558f487baf1dc69b5fe8a377d246d5f95eafbf66
Filip Rachůnek <Filip.Rachunek@gmail.com>
2025-12-14 20:43:42 +0100

    PMD rest.

--- src/main/java/com/brainking/tools/utils/Fonts.java
+++ src/main/java/com/brainking/tools/utils/Fonts.java
@@ -2,12 +2,13 @@ package com.brainking.tools.utils;
 
 import java.awt.*;
 
+@SuppressWarnings({"PMD.DataClass"})
 public class Fonts {
 
-    public static Font METADATA_FONT = new Font(Font.SERIF, Font.PLAIN, 25);
-    public static Font ROW_COLUMN_FONT = new Font(Font.MONOSPACED, Font.PLAIN, 40);
-    public static Font RESULT_FONT = new Font(Font.SERIF, Font.PLAIN, 30);
-    public static Font LINE_FONT = new Font("Arial Narrow", Font.PLAIN, 15);
-    public static Font SYMBOL_FONT = new Font(Font.SERIF, Font.PLAIN, 40);
+    public static final Font METADATA_FONT = new Font(Font.SERIF, Font.PLAIN, 25);
+    public static final Font ROW_COLUMN_FONT = new Font(Font.MONOSPACED, Font.PLAIN, 40);
+    public static final Font RESULT_FONT = new Font(Font.SERIF, Font.PLAIN, 30);
+    public static final Font LINE_FONT = new Font("Arial Narrow", Font.PLAIN, 15);
+    public static final Font SYMBOL_FONT = new Font(Font.SERIF, Font.PLAIN, 40);
 
 }
